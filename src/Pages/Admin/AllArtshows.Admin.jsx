713ef039933e8318b2dd67d6437073b4c6d34ded
Jacob Wazydrag <jgw.jakegeorge@gmail.com>
2022-08-26 15:00:12 -0500

    dev to qa merger

--- src/Pages/Admin/AllArtshows.Admin.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AllArtshowsAdmin() {
-  return (
-    <div>AllArtshows.Admin</div>
-  )
-}
--- src/Pages/User/ArtshowDetail.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ArtshowDetail() {
-  return (
-    <div>ArtshowDetail</div>
-  )
-}
