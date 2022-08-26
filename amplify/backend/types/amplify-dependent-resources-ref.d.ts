b05ba8184ee5fce5e0d309b834db5714b412ccc7
Jacob Wazydrag <jgw.jakegeorge@gmail.com>
2022-08-26 15:26:50 -0500

    connected backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -11,5 +11,30 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "s3udemyawsamplifyreactstorage48843bf6": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "udemyawsamplifyreact": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        },
+        "AdminQueries": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "function": {
+        "AdminQueries01ecf446": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import React from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+// import { Routes, Route } from 'react-router-dom';
+// import Artshow from './Pages/User/Artshow';
+// import ArtshowDetail from './Pages/User/ArtshowDetail';
+// import Artwork from './Pages/User/Artwork';
+// import ArtworkDetail from './Pages/User/ArtworkDetail';
+// import Home from './Pages/User/Home';
+import { createTheme, ThemeProvider } from '@mui/material';
+// import Layout from './Components/Layout';
+// import Profile from './Pages/User/Profile';
+// import ArtworkUpload from './Pages/User/ArtworkUpload';
+// import AllArtistsAdmin from './Pages/Admin/AllArtists.Admin';
+// import AllArtshowsAdmin from './Pages/Admin/AllArtshows.Admin';
+// import AllArtworksAdmin from './Pages/Admin/AllArtworks.Admin';
+// import HomeAdmin from './Pages/Admin/Home.Admin';
+// import LayoutAdmin from './Components/Layout.Admin';
+// import AllArtistDetailAdmin from './Pages/Admin/AllArtistDetail.Admin';
+// import AllArtworksDetailAdmin from './Pages/Admin/AllArtworksDetail.Admin';
+// import ProfileAdmin from './Pages/Admin/Profile.Admin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the TEST Environment!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TEST
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsExports);
+
+export default function App() {
+    const theme = createTheme({});
+    return (
+        // <div>hello</div>
+        <Authenticator socialProviders={['google', 'facebook', 'amazon']}>
+            {({ signOut, user }) =>
+                user.getSignInUserSession().getAccessToken().payload[
+                    'cognito:groups'
+                ] &&
+                user
+                .getSignInUserSession()
+                .getAccessToken()
+                .payload['cognito:groups'].includes('Admin') ? (
+                    <ThemeProvider theme={theme}>
+                        {console.log('user', user)}
+                        {/* <LayoutAdmin user={user} signout={signOut}>
+                            <Routes>
+                                <Route path='/' element={<HomeAdmin />} />
+                                <Route
+                                    path='/all-artshows'
+                                    element={<AllArtshowsAdmin />}
+                                />
+                                <Route
+                                    path='/artshow-detail/:id'
+                                    element={<ArtshowDetail />}
+                                />
+                                <Route
+                                    path='/all-artworks'
+                                    element={<AllArtworksAdmin />}
+                                />
+                                <Route
+                                    path='/all-artworks/:id'
+                                    element={<AllArtworksDetailAdmin />}
+                                />
+                                <Route
+                                    path='/all-artists'
+                                    element={<AllArtistsAdmin />}
+                                />
+                                <Route
+                                    path='/all-artists/:id'
+                                    element={<AllArtistDetailAdmin />}
+                                />
+                                <Route
+                                    path='/admin-profile'
+                                    element={<ProfileAdmin />}
+                                />
+                            </Routes>
+                        </LayoutAdmin> */}
+                    </ThemeProvider>
+                ) : (
+                    <ThemeProvider theme={theme}>
+                        {/* <Layout user={user} signout={signOut}>
+                            <Routes>
+                                <Route path='/' element={<Home />} />
+                                <Route path='/artshow' element={<Artshow />} />
+                                <Route
+                                    path='/artshow-detail/:id'
+                                    element={<ArtshowDetail />}
+                                />
+                                <Route path='/artwork' element={<Artwork />} />
+                                <Route
+                                    path='/artwork/:id'
+                                    element={<ArtworkDetail />}
+                                />
+                                <Route path='/profile' element={<Profile />} />
+                                <Route
+                                    path='/upload-artwork'
+                                    element={<ArtworkUpload />}
+                                />
+                            </Routes>
+                        </Layout> */}
+                    </ThemeProvider>
+                )
+            }
+        </Authenticator>
+    );
 }
-
-export default App;
