9af05476d0ff14f3fc24671e2f5b29f531b40f60
Jacob Wazydrag <jgw.jakegeorge@gmail.com>
2022-08-26 20:32:31 -0500

    testing User Routes artwork first

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import React from 'react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 // import Artshow from './Pages/User/Artshow';
 // import ArtshowDetail from './Pages/User/ArtshowDetail';
-// import Artwork from './Pages/User/Artwork';
-// import ArtworkDetail from './Pages/User/ArtworkDetail';
-// import Home from './Pages/User/Home';
+import Artwork from './Pages/User/Artwork';
+import ArtworkDetail from './Pages/User/ArtworkDetail';
+import Home from './Pages/User/Home';
 import { createTheme, ThemeProvider } from '@mui/material';
 import Layout from './Components/Layout';
 // import Profile from './Pages/User/Profile';
@@ -77,24 +77,24 @@ export default function App() {
                 ) : (
                     <ThemeProvider theme={theme}>
                         <Layout user={user} signout={signOut}>
-                            {/* <Routes>
+                            <Routes>
                                 <Route path='/' element={<Home />} />
-                                <Route path='/artshow' element={<Artshow />} />
+                                {/* <Route path='/artshow' element={<Artshow />} />
                                 <Route
                                     path='/artshow-detail/:id'
                                     element={<ArtshowDetail />}
-                                />
+                                /> */}
                                 <Route path='/artwork' element={<Artwork />} />
                                 <Route
                                     path='/artwork/:id'
                                     element={<ArtworkDetail />}
                                 />
-                                <Route path='/profile' element={<Profile />} />
+                                {/* <Route path='/profile' element={<Profile />} />
                                 <Route
                                     path='/upload-artwork'
                                     element={<ArtworkUpload />}
-                                />
-                            </Routes> */}
+                                /> */}
+                            </Routes>
                         </Layout>
                     </ThemeProvider>
                 )
---
+++ src/Pages/User/ArtworkImage.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { Storage } from 'aws-amplify';
+import ImageGallery from 'react-image-gallery';
+import _ from 'underscore';
+import 'react-image-gallery/styles/css/image-gallery.css';
+
+export default function ArtworkImage(props) {
+    const [imageUrls, setImgUrls] = useState([]);
+    useEffect(() => {
+        getUrls();
+    }, [props.detail]);
+
+    const returnMyUrlsForMainImage = async (imageUrl) => {
+        return new Promise((resolve, reject) => {
+            Storage.get(imageUrl, { level: 'public' })
+                .then((url) =>
+                    resolve({
+                        original: url,
+                        thumbnail: url
+                    })
+                )
+                .catch((err) => console.log(err));
+        });
+    };
+
+    const getUrls = async () => {
+        let promises = [];
+        _.forEach(props.detail, (value, key) => {
+            if (key.includes('image')) {
+                return promises.push(returnMyUrlsForMainImage(value.key));
+            }
+        });
+
+        Promise.all(promises)
+            .then((imageUrls) => {
+                setImgUrls(imageUrls);
+            })
+            .catch((err) => {
+                console.log('error', err);
+            });
+    };
+    
+    return (
+        <div style={{maxHeight: 600, maxWidth: 600}}>
+            {imageUrls.length === 0 ? null : (
+                // <div>Test</div>
+                <ImageGallery
+                    showBullets={true}
+                    showIndex={true}
+                    showThumbnails={true}
+                    lazyLoad={true}
+                    showPlayButton={false}
+                    items={imageUrls}
+                />
+            )}
+        </div>
+    );
+}
